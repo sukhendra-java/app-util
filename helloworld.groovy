aa418a2189b915eb12932693fe1b9ad2cf776cd7
sukhendra-java <67363140+sukhendra-java@users.noreply.github.com>
2024-07-13 20:50:54 +0530

    Update helloworld.groovy

--- helloworld.groovy
+++ helloworld.groovy
@@ -1,10 +1,16 @@
-#!groovy
+#!/usr/bin/groovy
 
 def firstTest(){
+
+  node('test-node'){
+
     stage('build'){
+      echo "Second stage"
     }
+
     stage('Cleanup workspace'){
       deleteDir()
     }
+  }
 }
 return this
